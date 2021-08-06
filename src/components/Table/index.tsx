ecf89ec987a015302559680669544d9905028dc0
Mateus Souza <mateus_souza_teles@hotmail.com>
2021-08-05 21:54:16 -0300

    Create component Table

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,63 @@
+import { transparentize } from "polished";
+import { TableInstance } from "react-table";
+import { NoData } from "../NoData";
+import {
+  Body,
+  BodyCell,
+  BodyRow,
+  Heading,
+  HeadingCell,
+  HeadingRow,
+  Wrapper,
+} from "./styles";
+
+interface TableProps<T extends Object> {
+  instance: TableInstance<T>;
+}
+
+export function Table<T extends Object>({ instance }: TableProps<T>) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = instance;
+
+  return (
+    <>
+      <Wrapper cellPadding={0} cellSpacing={0} {...getTableProps()}>
+        <Heading>
+          {headerGroups.map((headerGroup) => (
+            <HeadingRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <HeadingCell {...column.getHeaderProps}>
+                  {column.render("Header")}
+                </HeadingCell>
+              ))}
+            </HeadingRow>
+          ))}
+        </Heading>
+        <Body {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <BodyRow {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <BodyCell {...cell.getCellProps()}>
+                    {cell.render("Cell")}
+                  </BodyCell>
+                ))}
+              </BodyRow>
+            );
+          })}
+        </Body>
+      </Wrapper>
+      {!rows.length && (
+        <div style={{ backgroundColor: transparentize(0.95, "#274060") }}>
+          <NoData height={360} />
+        </div>
+      )}
+    </>
+  );
+}
