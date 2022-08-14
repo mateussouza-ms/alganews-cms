2ee6016f382aed69b9aed9aa149f1ce2de82d16a
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-08-13 22:16:45 -0300

    Create MarkdownEditor component

---
+++ src/stories/MarkdownEditor.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { MarkdownEditor } from "../components/MarkdownEditor";
+
+export default {
+  title: "Example/MarkdownEditor",
+  component: MarkdownEditor,
+} as ComponentMeta<typeof MarkdownEditor>;
+
+const Template: ComponentStory<typeof MarkdownEditor> = (args) => (
+  <MarkdownEditor />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
