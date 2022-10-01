23f992b961ca0556c1a072fd9db1eeb6967007bd
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-09-30 23:10:23 -0300

    check untreated rejections

---
+++ src/app/index.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { info } from "../core/utils/info";
+import { EditorProfileView } from "./views/EditorProfileView";
+import { EditorsListView } from "./views/EditorsListView";
+import { HomeView } from "./views/HomeView";
+import { NotFound404View } from "./views/NotFound404View";
+import { PostCreateView } from "./views/PostCreateView";
+
+export function App() {
+  useEffect(() => {
+    window.onunhandledrejection = function (error) {
+      console.log("error", error);
+      info({
+        title: error.reason.response?.data.title || "Erro inesperado",
+        content:
+          error.reason.response?.data.detail ||
+          error.reason.message ||
+          "Houve um erro inesperado.",
+      });
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/editores" element={<EditorsListView />} />
+        <Route path="/editores/:id" element={<EditorProfileView />} />
+        <Route path="/posts/criar" element={<PostCreateView />} />
+
+        <Route path="*" element={<NotFound404View />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { EditorProfileView } from "./app/views/EditorProfileView";
-import { EditorsListView } from "./app/views/EditorsListView";
-import { HomeView } from "./app/views/HomeView";
-import { NotFound404View } from "./app/views/NotFound404View";
-import { PostCreateView } from "./app/views/PostCreateView";
+import { App } from "./app";
+
 import GlobalStyles from "./core/globalStyles";
 import "./core/imports.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/editores" element={<EditorsListView />} />
-        <Route path="/editores/:id" element={<EditorProfileView />} />
-        <Route path="/posts/criar" element={<PostCreateView />} />
-
-        <Route path="*" element={<NotFound404View />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
     <GlobalStyles />
   </React.StrictMode>,
   document.getElementById("root")
