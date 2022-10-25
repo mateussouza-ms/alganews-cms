1c7f178b1683e0cd53c998c4113e86e72c35566b
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-10-25 20:49:27 -0300

    Finalize post preview

--- src/app/components/PostPreview/index.tsx
+++ src/app/components/PostPreview/index.tsx
@@ -1,7 +1,8 @@
 import { withBoundary } from "../../../core/hoc/withBoundary";
+import { Button } from "../Button";
 import { MarkdownEditor } from "../MarkdownEditor";
 
-import { Wrapper } from "./styles";
+import { Actions, Content, Heading, Image, Title, Wrapper } from "./styles";
 
 interface PostPreviewProps {
   postId: number;
@@ -10,8 +11,26 @@ interface PostPreviewProps {
 function PostPreview(props: PostPreviewProps) {
   return (
     <Wrapper>
-      features/PostPreview
-      <MarkdownEditor readOnly value={"ola mundo\n- esta é\n- uma lista"} />
+      <Heading>
+        <Title>{"Como fiquei rico aprendendo React"}</Title>
+        <Actions>
+          <Button variant={"danger"} label={"Publicar"} />
+          <Button variant={"primary"} label={"Editar"} />
+        </Actions>
+      </Heading>
+      <Image
+        src={
+          "https://images.unsplash.com/photo-1499343628900-545067aef5a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
+        }
+      />
+      <Content>
+        <MarkdownEditor
+          readOnly
+          value={
+            "Olá mundo!\n- Esta é\n- uma lista\n- uma lista\n- uma lista\n- uma lista\n"
+          }
+        />
+      </Content>
     </Wrapper>
   );
 }
--- src/app/components/PostPreview/styles.ts
+++ src/app/components/PostPreview/styles.ts
@@ -1,11 +1,37 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
-  width: 655px;
   background-color: #f3f8fa;
-  border: 1px solid #ccc;
   padding: 24px;
+  border: 1px solid #ccc;
+  width: 655px;
+  display: flex;
+  gap: 24px;
+  flex-direction: column;
+  max-height: 70vh;
+  overflow-y: auto;
+  box-shadow: 0 6px 6px rgba(0, 0, 0, 0.05);
 `;
+
+export const Heading = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h2`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const Image = styled.img`
+  height: 240px;
+  width: 100%;
+  object-fit: cover;
+`;
+
+export const Content = styled.div``;
