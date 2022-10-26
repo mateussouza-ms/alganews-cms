f4893e0057abd7ca629db04179287c48caf3f7c2
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-10-25 22:06:56 -0300

    Open links in new tab on MarkdownEditor

--- src/app/components/MarkdownEditor/index.tsx
+++ src/app/components/MarkdownEditor/index.tsx
@@ -6,6 +6,23 @@ MdEditor.unuse(Plugins.FontUnderline);
 
 const parser = new MarkdownIt();
 
+const defaultRender =
+  parser.renderer.rules.link_open ||
+  function (tokens: any, idx: any, options: any, env: any, self: any) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+parser.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  var aIndex = tokens[idx].attrIndex("target");
+  if (aIndex < 0) {
+    tokens[idx].attrPush(["target", "_blank"]);
+  } else {
+    //@ts-ignore
+    tokens[idx].attrs[aIndex][1] = "_blank";
+  }
+  return defaultRender(tokens, idx, options, env, self);
+};
+
 interface MarkdownEditorProps {
   onChange?: (markdownText: string) => void;
   value?: string;
